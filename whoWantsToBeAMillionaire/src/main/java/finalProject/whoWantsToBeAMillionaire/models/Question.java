e0d3238021ffaf200406cfb9ec6723054e140a12
crispymccoy145 <samspam19941@gmail.com>
2021-06-29 10:19:58 +0100

    question class created

--- whoWantsToBeAMillionaire/src/main/java/finalProject/whoWantsToBeAMillionaire/models/Question.java
+++ whoWantsToBeAMillionaire/src/main/java/finalProject/whoWantsToBeAMillionaire/models/Question.java
@@ -1,5 +1,52 @@
 package finalProject.whoWantsToBeAMillionaire.models;
 
+import java.util.List;
+
 public class Question {
 
+    private Long id;
+    private String question;
+    private List<Answer> answers;
+    private Difficulty difficulty;
+
+    public Question(String question, List<Answer> answers, Difficulty difficulty) {
+        this.question = question;
+        this.answers = answers;
+        this.difficulty = difficulty;
+    }
+
+    public Question() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public List<Answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<Answer> answers) {
+        this.answers = answers;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
 }
