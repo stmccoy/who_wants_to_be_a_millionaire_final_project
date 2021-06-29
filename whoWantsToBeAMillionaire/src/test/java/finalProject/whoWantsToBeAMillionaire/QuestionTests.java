26bd001bdc97433d35c4a33d6a2d51ea5abd75cf
crispymccoy145 <samspam19941@gmail.com>
2021-06-29 10:50:33 +0100

    question tests completed

--- whoWantsToBeAMillionaire/src/test/java/finalProject/whoWantsToBeAMillionaire/QuestionTests.java
+++ whoWantsToBeAMillionaire/src/test/java/finalProject/whoWantsToBeAMillionaire/QuestionTests.java
@@ -13,7 +13,12 @@ import static org.junit.Assert.assertEquals;
 
 public class QuestionTests {
 
-    public  Question question;
+    private  Question question;
+    private List<Answer> answers;
+    private Answer answerOne;
+    private Answer answerTwo;
+    private Answer answerThree;
+    private Answer answerFour;
 
     @Before
     public void before(){
@@ -33,5 +38,41 @@ public class QuestionTests {
     @Test
     public void questionHasAnswersArray(){
         assertEquals(0, question.getAnswers().size());
+    }
+
+    @Test
+    public void canSetQuestionAnswersArray(){
+        //arrange
+        answerOne = new Answer("London", true);
+        answerTwo = new Answer("Manchester", false);
+        answerThree = new Answer("Bristol", false);
+        answerFour = new Answer("Edinburgh", false);
+        answers = new ArrayList<>();
+        answers.add(answerOne);
+        answers.add(answerTwo);
+        answers.add(answerThree);
+        answers.add(answerFour);
+
+        //act
+        question.setAnswers(answers);
+
+        //assert
+        assertEquals(4, question.getAnswers().size());
+        assertEquals(true, question.getAnswers().contains(answerOne));
+        assertEquals(true, question.getAnswers().contains(answerTwo));
+        assertEquals(true, question.getAnswers().contains(answerThree));
+        assertEquals(true, question.getAnswers().contains(answerFour));
+    }
+
+    @Test
+    public void canSetQuestion(){
+        question.setQuestion("What is the distance between India and Nepal");
+        assertEquals("What is the distance between India and Nepal", question.getQuestion());
+    }
+
+    @Test
+    public void canSetDifficulty(){
+        question.setDifficulty(Difficulty.TWO);
+        assertEquals(Difficulty.TWO, question.getDifficulty());
     }
 }
