e2bbfd91647a4559cde7dd08dad24ed3f79d6553
crispymccoy145 <samspam19941@gmail.com>
2021-06-29 10:15:56 +0100

    answer class created

--- whoWantsToBeAMillionaire/src/main/java/finalProject/whoWantsToBeAMillionaire/models/Answer.java
+++ whoWantsToBeAMillionaire/src/main/java/finalProject/whoWantsToBeAMillionaire/models/Answer.java
@@ -2,4 +2,39 @@ package finalProject.whoWantsToBeAMillionaire.models;
 
 public class Answer {
 
+    private Long id;
+    private String answer;
+    private boolean correct;
+
+    public Answer(String answer, boolean correct) {
+        this.answer = answer;
+        this.correct = correct;
+    }
+
+    public Answer() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
+    }
 }
