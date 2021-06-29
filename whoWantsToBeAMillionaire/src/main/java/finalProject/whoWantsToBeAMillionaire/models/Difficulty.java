6c861d84fa51ab61018505770bc61a94bed8eb73
crispymccoy145 <samspam19941@gmail.com>
2021-06-29 10:17:53 +0100

    enum for difficulty made

--- whoWantsToBeAMillionaire/src/main/java/finalProject/whoWantsToBeAMillionaire/models/Difficulty.java
+++ whoWantsToBeAMillionaire/src/main/java/finalProject/whoWantsToBeAMillionaire/models/Difficulty.java
@@ -1,4 +1,16 @@
 package finalProject.whoWantsToBeAMillionaire.models;
 
 public enum Difficulty {
+
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT;
+
+    Difficulty() {
+    }
 }
