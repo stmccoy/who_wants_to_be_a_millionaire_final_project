bd8c756298dc0f952fecb4eb75430b0035019965
crispymccoy145 <samspam19941@gmail.com>
2021-07-03 11:49:50 +0100

    added life lines logos

--- whoWantsToBeAMillionaireFrontEnd/who-wants-to-be-a-millionaire-react-app/src/components/mainPageContainerComponents/LifeLinesComponent.js
+++ whoWantsToBeAMillionaireFrontEnd/who-wants-to-be-a-millionaire-react-app/src/components/mainPageContainerComponents/LifeLinesComponent.js
@@ -3,7 +3,15 @@ import React from 'react';
 function LifeLinesComponent(){
     return (
         <aside className="life-lines">
-            Life lines
+            <div>
+                <img src="./images/Classic5050.png" className="life-lines-pictures" alt="50-50 logo" id="50-50-logo"/>
+            </div>
+            <div>
+                <img src="./images/ClassicATA.png" className="life-lines-pictures" alt="Ask The Audience logo" id="ask-the-audience-logo"/>
+            </div>
+            <div>
+                <img src="./images/ClassicPAF.png" className="life-lines-pictures" alt="Phone A Friend Logo" id="phone-a-friend-logo"/>
+            </div>
         </aside>
     )
 }
